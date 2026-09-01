eb2df11143050f03160b6e091b4a11f68ca53744
ggm77 <shmshm86@naver.com>
2026-09-01 14:05:42 +0900

    first commit

---
+++ src/main/java/com/seohamin/hisnetmobile/HisnetmobileApplication.java
@@ -0,0 +1,13 @@
+package com.seohamin.hisnetmobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HisnetmobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HisnetmobileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seohamin/hisnetmobile/HisnetmobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seohamin.hisnetmobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HisnetmobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
